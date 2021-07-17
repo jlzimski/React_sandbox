a4e14e0ee346f3cbcb5b75b556a0b1450d1071ca
jlzimski <jlzimski@gmail.com>
2021-07-17 13:36:56 -0400

    added components to home

--- src/components/site/Home.js
+++ src/components/site/Home.js
@@ -3,6 +3,12 @@ import FeatureCard from '../components/FeatureCard';
 import ListGroups from '../components/ListGroups';
 import Carousel from '../components/Carousel';
 import Forms from '../components/Forms';
+import Jumbotron  from '../components/Jumbotron';
+import Layout from '../components/Layout';
+import OtherButtons from '../components/OtherButtons';
+import Popovers from '../components/Popovers';
+import ResponsiveTable from '../components/ResponsiveTable';
+import ViewContent from '../components/ViewContent'
 
 const Home = () => {
     return (
@@ -13,6 +19,12 @@ const Home = () => {
                 <FeatureCard />
                 <ListGroups />
                 <Carousel />
+                <Jumbotron />
+                <Layout />
+                <OtherButtons />
+                <Popovers />
+                <ResponsiveTable />
+                <ViewContent />
             </div>
         </div>
     );
